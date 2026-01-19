a94ffc52c6ce42b7b961aad21aff8c1d52a51dac
Bryan <bryanwagoki2016@gmail.com>
2026-01-19 05:58:43 +0300

    test: use describe method to scope my inner and outer tests

--- src/testing.test.js
+++ src/testing.test.js
@@ -14,4 +14,37 @@ test("increment twice", () => {
   expect(count).toBe(2);
 });
 
+let items = [];
 
+beforeEach(() => {
+  items = [];
+});
+
+test("adds item", () => {
+  items.push("apple");
+  expect(items.length).toBe(1);
+});
+
+test("starts empty", () => {
+  expect(items.length).toBe(0);
+});
+
+let counted = 0;
+
+beforeEach(() => {
+  counted = 1;
+});
+
+describe("inner block", () => {
+  beforeEach(() => {
+    counted = 2;
+  });
+
+  test("test A", () => {
+    expect(counted).toBe(2);
+  });
+});
+
+test("test B", () => {
+  expect(counted).toBe(1);
+});
