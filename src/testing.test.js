0f49409a6504d5b911127bcc364fe404c5f55e92
Bryan <bryanwagoki2016@gmail.com>
2026-01-19 05:15:53 +0300

    test: do a setup with beforeEach method

--- src/testing.test.js
+++ src/testing.test.js
@@ -1,3 +1,17 @@
-import sum from "./index.js";
+let count = 0;
+
+beforeEach(() => {
+  count = 0;
+});
+
+test("increment once", () => {
+  count++;
+  expect(count).toBe(1);
+});
+
+test("increment twice", () => {
+  count += 2;
+  expect(count).toBe(2);
+});
 
 
